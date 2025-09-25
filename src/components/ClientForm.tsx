6477862bf451d7ef52db481310cbf992f4620919
Br7eleven <balajhussain1122@gmail.com>
2025-09-25 12:23:55 -0700

    updated clientform

--- src/components/ClientForm.tsx
+++ src/components/ClientForm.tsx
@@ -97,7 +97,7 @@ export function ClientForm({ onSubmit }: ClientFormProps) {
           type="submit"
           className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
         >
-          Add Client
+          Add Customer
         </button>
       </div>
     </form>
